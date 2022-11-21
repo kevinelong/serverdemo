3b0ccbdadd66a61e9fd1e9107d7c4a02ed8a7b67
Kevin Ernest Long <kevinelong@gmail.com>
2022-11-21 19:02:47 +0000

    buy command

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require('fs');
+var gravatar = require('gravatar');
 
 let users = JSON.parse(fs.readFileSync('users.json'));
 console.log(users);
@@ -8,9 +9,9 @@ const app = express();
 
 app.use(express.json({extended: true, limit: '1mb'}));
 
-app.get('/',(req, res) => res.sendFile("index.html") );
-app.get('/register',(req, res) => res.sendFile("register.html") );
-app.get('/chat',(req, res) => res.sendFile("chat.html") );
+app.get('/',(req, res) => res.sendfile("index.html") );
+app.get('/register',(req, res) => res.sendfile("register.html") );
+app.get('/chat',(req, res) => res.sendfile("chat.html") );
 
 app.post('/login',(req, res) => {
   const user = users.find(u => req.body.username === u.username && req.body.password === u.password);
@@ -18,6 +19,7 @@ app.post('/login',(req, res) => {
 });
 
 app.post('/register',(req, res) => {
+  req.body.image = gravatar.url(req.body.username);
   users.push(req.body);
   fs.writeFileSync('users.json', JSON.stringify(users));
   res.json({result:"success", data: req.body});
@@ -28,6 +30,20 @@ app.get('/messages',(req, res) => {
 });
 
 app.post('/messages',(req, res) => {
+  if(req.body.message.length <= 0){
+    res.json({result:"fail", data: req.body});
+  }
+  
+  if("/" == req.body.message.charAt(0)){
+    let command = req.body.message.slice(1,req.body.message.length);
+    let remaining = req.body.message.slice(command.indexOf(" ")+1, req.body.message.length);
+    let words = command.split(" ");
+    if(words[0] == "buy"){
+      req.body.message = `${req.body.username} buys ${remaining}.`;
+      req.body.username = "SYSTEM";
+    }
+  }
+  
   let messages = JSON.parse(fs.readFileSync('messages.json'));
   messages.push(req.body);
   fs.writeFileSync('messages.json', JSON.stringify(messages));
