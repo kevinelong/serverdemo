90b3e4952d76755ef46e78d48da9edc1e350d0eb
Kevin Ernest Long <kevinelong@gmail.com>
2022-11-21 20:20:11 +0000

    items

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 var gravatar = require('gravatar');
 
 let users = JSON.parse(fs.readFileSync('users.json'));
-console.log(users);
+let items = JSON.parse(fs.readFileSync('items.json'));
 
 const app = express();
 
@@ -20,6 +20,8 @@ app.post('/login',(req, res) => {
 
 app.post('/register',(req, res) => {
   req.body.image = gravatar.url(req.body.username);
+  req.body.coin = 10000;
+  req.body.inventory = [];
   users.push(req.body);
   fs.writeFileSync('users.json', JSON.stringify(users));
   res.json({result:"success", data: req.body});
@@ -36,10 +38,28 @@ app.post('/messages',(req, res) => {
   
   if("/" == req.body.message.charAt(0)){
     let command = req.body.message.slice(1,req.body.message.length);
-    let remaining = req.body.message.slice(command.indexOf(" ")+1, req.body.message.length);
+    let remaining = req.body.message.slice(command.indexOf(" ")+2, req.body.message.length);
     let words = command.split(" ");
+    
     if(words[0] == "buy"){
-      req.body.message = `${req.body.username} buys ${remaining}.`;
+      const item = items.find(o => o.name === remaining );
+      if(item){
+        const user = users.find(u => u.username === req.body.username );
+        if(user.coin >= item.price){
+          req.body.message = `${req.body.username} buys ${item.name} for ${item.price}.`;   
+          user.coin -= item.price;
+          user.inventory.push(item);
+          fs.writeFileSync('users.json', JSON.stringify(users));
+        }else{
+          req.body.message = `${user.coin} cannot buy ${item.name} for ${item.price}.`;   
+        }
+      }else{
+        req.body.message = `No such item "${remaining}".`;
+      }
+      req.body.username = "SYSTEM";
+    }else if(words[0] == "inv"){
+      const user = users.find(u => u.username === req.body.username );
+      req.body.message = `INVENTORY "${  user.inventory.map(i=>i.name).join(", ") }".`;
       req.body.username = "SYSTEM";
     }
   }
@@ -48,6 +68,7 @@ app.post('/messages',(req, res) => {
   messages.push(req.body);
   fs.writeFileSync('messages.json', JSON.stringify(messages));
   res.json({result:"success", data: req.body});
+
 });
 
 app.listen(3000,() => {
